568b6c1e1463958391017dc52993cb67c7172564
github username <github email address>
2017-06-28 17:03:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function Dog(name, breed, age){
+  this.name = name
+  this.breed = breed
+  this.age = age
+}
+
+function Cat(name, breed, age){
+  this.name = name
+  this.breed = breed
+  this.age = age
+}
+
+function Ferret(name, age, foodRequirements){
+  this.name = name
+  this.foodRequirements = foodRequirements
+  this.age = age
+}
+
+function Fish(name, species, waterConditions){
+  this.name = name
+  this.waterConditions = waterConditions
+  this.species = species
+}
+
+function Bird(name, breed, foodRequirements){
+  this.name = name
+  this.breed = breed
+}
